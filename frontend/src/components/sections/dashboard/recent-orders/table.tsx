e81058e76aefee9caf02038dae224352e0e36670
PRATHAM KUMAR MISHRA <pratham.mishra@students.iiit.ac.in>
2025-02-12 00:49:12 +0530

    fixed

--- frontend/src/components/sections/dashboard/recent-orders/table.tsx
+++ frontend/src/components/sections/dashboard/recent-orders/table.tsx
@@ -10,7 +10,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography';
-import { fetchTopDataCollectors } from '/services/api';
+import { fetchTopDataCollectors } from 'services/api';
 
 interface DataCollector {
   name: string;
--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,9 +1,10 @@
 import Grid from '@mui/material/Grid';
 import Analytics from 'components/sections/dashboard/analytics';
-import RecentOrders from 'components/sections/dashboard/recent-orders';
+// import RecentOrders from 'components/sections/dashboard/recent-orders';
 import Reports from 'components/sections/dashboard/reports';
 import TopCards from 'components/sections/dashboard/top-cards';
 // import TopSelling from 'components/sections/dashboard/top-selling';
+import Table from 'components/sections/dashboard/recent-orders/table';
 import Table2 from 'components/sections/dashboard/recent-orders/table2';
 import Table3 from 'components/sections/dashboard/recent-orders/table3';
 
@@ -19,8 +20,11 @@ const Dashboard = () => {
       <Grid item xs={12} md={5}>
         <Analytics />
       </Grid>
-      <Grid item xs={12} md={4}>
+      {/* <Grid item xs={12} md={4}>
         <RecentOrders />
+      </Grid> */}
+      <Grid item xs={2} md={4}>
+        <Table />
       </Grid>
       <Grid item xs={2} md={4}>
         <Table2 />
